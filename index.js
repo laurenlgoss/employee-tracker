92001c1221ddc30b8aecd8ad4fbe491f7293e44b
Lauren Goss <laurenlgoss98@gmail.com>
2021-08-25 13:39:31 -0400

    added workplace_db schema

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const mysql = require("mysql2");
 
-// Connect to employees_db database
-const employeesDatabase = mysql.createConnection(
+// Connect to workplace_db database
+const workplaceDatabase = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'employees_db'
+        database: 'workplace_db'
     },
-    console.log(`Connected to the employees_db database.`)
+    console.log(`Connected to the workplace_db database.`)
 );
