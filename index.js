7c6e569612b74a6a0a8ad46f1ed49e4b57d3c7f2
Lauren Goss <laurenlgoss98@gmail.com>
2021-08-29 11:28:42 -0400

    added functionality to add department

--- index.js
+++ index.js
@@ -14,12 +14,20 @@ const workplaceDatabase = mysql.createConnection(
 );
 
 // Create a question to ask user how they would like to interact with the database
-const question = [
+const MenuQuestion = [
     {
         type: "list",
         message: "What would you like to do?",
         choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add An Employee", "Update An Employee Role", "Quit", new inquirer.Separator()],
-        name: "choice",
+        name: "choice"
+    }
+];
+
+const addDepartmentQuestion = [
+    {
+        type: "input",
+        message: "Enter name of department: ",
+        name: "name"
     }
 ];
 
@@ -30,61 +38,72 @@ function init() {
 // Initialize inquirer session
 function mainMenu() {
     inquirer
-        .prompt(question)
+        .prompt(MenuQuestion)
         .then((response) => {
-            // If user chooses to view all departments,
-            if (response.choice === "View All Departments") {
-                // Render department table to terminal
-                renderTable("SELECT * FROM department LIMIT 100;");
+            if (response.choice !== "Quit") {
+                // If user chooses to view all departments,
+                if (response.choice === "View All Departments") {
+                    // Render department table to terminal
+                    renderTable("SELECT * FROM department LIMIT 100;");
 
-                return mainMenu();
-            }
-            // If user chooses to view all roles,
-            else if (response.choice === "View All Roles") {
-                // Join role and department tables, render table to terminal
-                renderTable("SELECT * FROM role JOIN department ON role.department_id = department.id;");
+                    return mainMenu();
+                }
+                // If user chooses to view all roles,
+                else if (response.choice === "View All Roles") {
+                    // Join role and department tables, render table to terminal
+                    renderTable("SELECT * FROM role JOIN department ON role.department_id = department.id;");
 
-                return mainMenu();
-            }
-            // If user chooses to view all employees,
-            else if (response.choice === "View All Employees") {
-                // Join employee and role tables, render table to terminal
-                renderTable("SELECT * FROM employee JOIN role ON employee.role_id = role.id;");
+                    return mainMenu();
+                }
+                // If user chooses to view all employees,
+                else if (response.choice === "View All Employees") {
+                    // Join employee and role tables, render table to terminal
+                    renderTable("SELECT * FROM employee JOIN role ON employee.role_id = role.id;");
 
-                return mainMenu();
-            }
-            // If user chooses to add a department,
-            else if (response.choice === "Add A Department") {
-                console.log("add department row");
+                    return mainMenu();
+                }
+                // If user chooses to add a department,
+                else if (response.choice === "Add A Department") {
+                    // Prompt user to enter department name
+                    inquirer
+                        .prompt(addDepartmentQuestion)
+                        .then((response) => {
+                            workplaceDatabase.query(`INSERT INTO department (name) VALUES ("${response.name}");`, function (err, results) {
+                                console.log(results);
+                                
+                                console.log(`${response.name} successfully inserted into department table.`)
 
-                return mainMenu();
-            }
-            // If user chooses to add a role,
-            else if (response.choice === "Add A Role") {
-                console.log("add role row");
+                                return mainMenu();
+                            });
+                        })
+                }
+                // If user chooses to add a role,
+                else if (response.choice === "Add A Role") {
+                    console.log("add role row");
 
-                return mainMenu();
-            }
-            // If user chooses to add an employee,
-            else if (response.choice === "Add An Employee") {
-                console.log("add employee row");
+                    return mainMenu();
+                }
+                // If user chooses to add an employee,
+                else if (response.choice === "Add An Employee") {
+                    console.log("add employee row");
 
-                return mainMenu();
-            }
-            // If user chooses to update an employee role,
-            else if (response.choice === "Update An Employee Role") {
-                console.log("change employee role");
+                    return mainMenu();
+                }
+                // If user chooses to update an employee role,
+                else if (response.choice === "Update An Employee Role") {
+                    console.log("change employee role");
 
-                return mainMenu();
+                    return mainMenu();
+                }
             }
             // If user chooses to quit,
-            else if (response.choice === "Quit") {
+            else {
                 return;
             }
         })
 }
 
-// Take in query, render given table to terminal
+// Take in MySQL query, render given table to terminal
 function renderTable(query) {
     workplaceDatabase.query(query, function (err, results) {
         console.log(`\n`);
